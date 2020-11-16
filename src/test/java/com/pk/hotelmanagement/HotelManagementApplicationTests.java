4b8caf8ed775761fce3c9043970099a8da6a72e8
Patryk <patry07@o2.pl>
2020-11-16 18:46:08 +0100

    edited .gitignore file

---
+++ src/test/java/com/pk/hotelmanagement/HotelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.hotelmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
